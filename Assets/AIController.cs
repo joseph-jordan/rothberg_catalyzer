ff77ad01a997ae944f0ff208d452032f48d2a324
Joseph Jordan <jjordan@gmail.com>
2019-10-19 17:12:01 -0400

    added ai and menu

---
+++ Assets/AIController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.IO;
+using System.IO.Ports;
+using UnityEngine.UI;
+using System.Threading;
+using System.Text;
+using System.Net;
+
+public class AIController : MonoBehaviour
+{
+    private float maxPos = 2.25f;
+    private float minPos = -2.25f;
+    private float xPos = 4.0f;
+    public GameObject Ball;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Ball == null)
+        {
+            Ball = GameObject.FindWithTag("Ball");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double ballY = Ball.GetComponent<Rigidbody2D>().position[1];
+        ballY = Math.Max(Math.Min(ballY, 7.75), -7.75);
+
+        double myY = this.GetComponent<Rigidbody2D>().position[1];
+
+        double diff = ballY - myY;
+        double random = GetRandomNumber(0.015, 0.23);
+        double boost = (0.16 * diff * random);
+        if (diff > 0.28 || diff < -0.28)
+        {
+            float pos = (float)((0.03738 * sign(diff) + boost) + myY);
+            if (pos < 0)
+            {
+                pos = Mathf.Max(pos, minPos);
+            }
+            pos = Mathf.Min(pos, maxPos);
+            transform.position = new Vector3(xPos, pos, 0.0f);
+        }
+        return;
+
+    }
+
+    public double GetRandomNumber(double minimum, double maximum)
+    {
+        System.Random random = new System.Random();
+        return random.NextDouble() * (maximum - minimum) + minimum;
+    }
+
+    int sign(double d)
+    {
+        if (d >= 0)
+        {
+            return 1;
+        }
+        return -1;
+    }
+}
---
+++ Assets/MenuControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    private string email;
+    private string password;
+
+
+    private void advance() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SetEmail(string newEmail) {
+        email = newEmail;
+    }
+
+    public void SetPassword(string newPassword) {
+        password = newPassword;
+    }
+
+    public void OnSubmit() {
+        advance();
+    }
+}
