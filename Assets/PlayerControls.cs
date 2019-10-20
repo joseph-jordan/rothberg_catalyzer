5c51a501a1bd3f092d1f27097c363dd4d74d437b
Joseph Jordan <jjordan@gmail.com>
2019-10-20 13:05:39 -0400

    added a bool that allows the developer to toggle between keyboard and device input, fixed a paddle pug

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -12,6 +12,7 @@ using System.Net;
 public class PlayerControls : MonoBehaviour {
 
 private float angle;
+private static bool useKeyboard = true;
     public static float globalMinAngle = float.MaxValue;
     public static float globalMaxAngle = float.MinValue;
     public static int totalHits;
@@ -24,15 +25,24 @@ private float xPos = -4.0f;
 public static SerialPort stream = new SerialPort("/dev/cu.usbmodem14101", 9600);
 public string userinput;
 private Thread streamReaderThread;
+public KeyCode moveUp = KeyCode.W;
+	public KeyCode moveDown = KeyCode.S;
+	public float speed = 10.0f;
+	public float boundY = 2.25f;
+	private Rigidbody2D rb2d;
 
 
 // Use this for initialization
 void Start () {
-stream.Open();
-stream.ReadTimeout = 15;
-streamReaderThread = new Thread(new ThreadStart(ReadInput));
-streamReaderThread.IsBackground = true;
-streamReaderThread.Start();
+    if (useKeyboard) {
+        rb2d = GetComponent<Rigidbody2D> ();
+    } else {
+        stream.Open();
+        stream.ReadTimeout = 15;
+        streamReaderThread = new Thread(new ThreadStart(ReadInput));
+        streamReaderThread.IsBackground = true;
+        streamReaderThread.Start();
+    }
 }
 
 void ReadInput() {
@@ -60,18 +70,38 @@ userinput = stream.ReadLine();
 
 // Update is called once per frame
 void Update () {
-float pos;
-float.TryParse (userinput, out angle);
-if (angle >= 0) {
-pos = -(angle / maxAngle) * maxPos ;
-} else {
-pos = -(angle / minAngle) * minPos ;
-}
-transform.position = new Vector3(xPos, Mathf.Clamp(pos,minPos,maxPos), 0.0f);
-}
+    if (useKeyboard) {
+        var vel = rb2d.velocity;
+		if (Input.GetKey (moveUp)) {
+			vel.y = speed;
+		} else if (Input.GetKey (moveDown)) {
+			vel.y = -speed;
+		} else if (!Input.anyKey) {
+			vel.y = 0;
+		}
+		rb2d.velocity = vel;
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        totalHits++;
+		var pos = transform.position;
+		if (pos.y > boundY) {
+			pos.y = boundY;
+		} else if (pos.y < -boundY) {
+			pos.y = -boundY;
+		}
+		transform.position = pos;
+    } else {
+        float pos;
+        float.TryParse (userinput, out angle);
+        if (angle >= 0) {
+        pos = -(angle / maxAngle) * maxPos ;
+        } else {
+        pos = -(angle / minAngle) * minPos ;
+        }
+        transform.position = new Vector3(xPos, Mathf.Clamp(pos,minPos,maxPos), 0.0f);
+        }
+
+            void OnCollisionEnter2D(Collision2D col)
+            {
+                totalHits++;
+            }
     }
 }
